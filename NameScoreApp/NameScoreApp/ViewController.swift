e68ef79e01a719459d9c332b6a16fff27a4460b8
batimiasu <olblisjjjjp@gmail.com>
2016-03-02 00:55:55 +0900

    Update nameScoreApp

--- NameScoreApp/NameScoreApp/ViewController.swift
+++ NameScoreApp/NameScoreApp/ViewController.swift
@@ -8,23 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.textField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.sendButton.sendActionsForControlEvents(.TouchUpInside)
+        return true
+    }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.textField.text = ""
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func showAlert() {
+        let VERSION: Float = (UIDevice.currentDevice().systemVersion as NSString).floatValue
+        if VERSION >= 8.0 {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your name", preferredStyle: .Alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alertController.addAction(defaultAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertView()
+            alert.title = "Error"
+            alert.message = "Please enter your name"
+            alert.addButtonWithTitle("OK")
+            alert.show()
+        }
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        if identifier == "showResult" {
+            if self.textField.text == "" {
+                self.showAlert()
+                return false
+            }
+            return true
+        }
+        return true
+    }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let resultViewController: ResultViewController = segue.destinationViewController as! ResultViewController
-        resultViewController.myName = self.textField.text!
+        if segue.identifier == "showResult" {
+            let resultViewController: ResultViewController = segue.destinationViewController as! ResultViewController
+            resultViewController.myName = self.textField.text!
+            self.textField.resignFirstResponder()
+        }
     }
 
 }
